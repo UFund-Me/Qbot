1942cb43d210d5b37563380072184e9b126339a6
Charmve <yidazhang1@gmail.com>
2022-12-01 00:23:50 +0800

    Add webpage demo to debug ci

---
+++ web/js/handle.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
